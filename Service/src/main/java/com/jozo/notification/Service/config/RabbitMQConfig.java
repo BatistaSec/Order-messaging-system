4cf677a43e49d777f87f602b33f7831c9cbde729
João Batista ⚙️ <joaojbsuzano@gmail.com>
2026-06-09 11:47:00 -0300

    Add files via upload

---
+++ Service/src/main/java/com/jozo/notification/Service/config/RabbitMQConfig.java
@@ -0,0 +1,44 @@
+package com.jozo.notification.Service.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.MessageConverter;
+@Configuration
+public class RabbitMQConfig {
+    public static final String QUEUE = "order.created.queue";
+    public static final String EXCHANGE = "order.exchange";
+    public static final String ROUTING_KEY = "order.created";
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+
+    @Bean
+    public DirectExchange orderExchange() {
+        return new DirectExchange(EXCHANGE);
+    }
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(QUEUE);
+    }
+    @Bean
+    public Binding binding(Queue orderQueue, DirectExchange orderExchange) {
+        return BindingBuilder.bind(orderQueue).to(orderExchange).with(ROUTING_KEY);
+    }
+}
